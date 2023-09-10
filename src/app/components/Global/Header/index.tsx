23235dc48edef5d312bf69e5f4e30221ff1d6078
ART1377 <alirezatt705@gmail.com>
2023-09-11 01:05:54 +0330

    dark mode toggle changed

--- src/app/components/Global/Header/index.tsx
+++ src/app/components/Global/Header/index.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Logo from "../Logo/Logo";
 import SearchBar from "./SearchBar";
 import style from "./Header.module.css";
@@ -11,14 +11,20 @@ type Props = {};
 
 const Header = (props: Props) => {
   const [showNavbar, setShowNavbar] = useState<boolean>(false);
-
+  
   const { systemTheme, theme, setTheme } = useTheme();
-    const currentTheme = theme === 'system' ? systemTheme : theme;
+  const [current,setCurrent]=useState<String>()
+  
+  useEffect(() => {
+      const currentTheme = theme === 'system' ? systemTheme : theme;
+      setCurrent(currentTheme)
+      
+    }, [systemTheme, theme]);
 
 
   return (
     <>
-      <header className={`sticky top-0 shadow-sm z-40 ${style.header} ${currentTheme == "dark"&&'border-b border-text-light'}`}>
+      <header className={`sticky top-0 shadow-sm z-40 ${style.header} ${current == "dark"&&'border-b border-text-light'}`}>
         <div className="flex justify-between items-center py-3 px-2 xs:px-3 sm:!px-5">
           <div
             className="md:!hidden !flex items-center cursor-pointer"
@@ -43,8 +49,8 @@ const Header = (props: Props) => {
           <Logo />
           <SearchBar />
           <div className={`flex items-center gap-2 ${style.icons}`}>
-            <div onClick={() => theme == "dark"? setTheme('light'): setTheme("dark")}>
-              <BsSun />
+            <div onClick={() => theme == "dark"? setTheme('light'): setTheme("dark")} className="overflow-hidden">
+              {current == "dark"?(<BsMoon className={`transform translate-y-6 transition-all duration-500 ${current=='dark'&&'translate-y-0'}`} />):(<BsSun className={`transform translate-y-6 transition-all duration-500 ${current=='light'&&'translate-y-0'}`} />)}
             </div>
             <div>
               <BsPerson />

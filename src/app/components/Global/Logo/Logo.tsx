f26fb18196eed4a251c675462681cbf0a3c220f5
ART1377 <alirezatt705@gmail.com>
2023-09-12 19:29:53 +0330

    scroll changed

--- src/app/components/Global/Logo/Logo.tsx
+++ src/app/components/Global/Logo/Logo.tsx
@@ -10,7 +10,7 @@ const Logo = (props: Props) => {
     <>
       <Link href={"/"}>
         <div className={`flex items-center ${style.container}`}>
-          <BsCameraReels className="text-lg xxs:text-xl me-1 mb-1" />
+          <BsCameraReels className="text-lg xxs:text-xl me-1 mb-2" />
           <h6 className="uppercase me-1 text-sm xxs:text-base">movie</h6>
           <h6 className="uppercase text-sm xxs:text-base">city</h6>
         </div>
--- src/app/components/Global/Share/index.tsx
+++ src/app/components/Global/Share/index.tsx
@@ -15,32 +15,32 @@ type Props = {};
 const Share = (props: Props) => {
   const shareUrl = window.location.href;
   return (
-    <div className="p-2 mt-6 flex flex-col gap-2">
-      <div className="border-2 p-2 text-text-dark rounded-2xl shadow-lg min-w-[180px] cursor-pointer">
+    <div className="p-2 mt-6 grid grid-cols-1 xs:grid-cols-2 gap-2 min-w-[240px] xxs:min-w-[280px] xs:min-w-[360px] sm:min-w-[500px]">
+      <div className="border-2 p-2 text-text-dark rounded-2xl shadow-lg min-w-[160px] xs:col-span-2 cursor-pointer">
         <EmailShareButton url={shareUrl} className="flex items-center justify-between w-full">
           <EmailIcon size={36} round />
         <p className="font-medium">Email</p>
         </EmailShareButton>
       </div>
-      <div className="border-2 p-2 text-text-dark rounded-2xl shadow-lg min-w-[180px] cursor-pointer">
+      <div className="border-2 p-2 text-text-dark rounded-2xl shadow-lg min-w-[160px] cursor-pointer">
         <FacebookShareButton url={shareUrl} className="flex items-center justify-between w-full">
           <FacebookIcon size={36} round />
         <p className="font-medium">Facebook</p>
         </FacebookShareButton>
       </div>
-      <div className="border-2 p-2 text-text-dark rounded-2xl shadow-lg min-w-[180px] cursor-pointer">
+      <div className="border-2 p-2 text-text-dark rounded-2xl shadow-lg min-w-[160px] cursor-pointer">
         <TelegramShareButton url={shareUrl} className="flex items-center justify-between w-full">
           <TelegramIcon size={36} round />
         <p className="font-medium">Telegram</p>
         </TelegramShareButton>
       </div>
-      <div className="border-2 p-2 text-text-dark rounded-2xl shadow-lg min-w-[180px] cursor-pointer">
+      <div className="border-2 p-2 text-text-dark rounded-2xl shadow-lg min-w-[160px] cursor-pointer">
         <WhatsappShareButton url={shareUrl} className="flex items-center justify-between w-full">
           <WhatsappIcon size={36} round />
         <p className="font-medium">Whatsapp</p>
         </WhatsappShareButton>
       </div>
-      <div className="border-2 p-2 text-text-dark rounded-2xl shadow-lg min-w-[180px] cursor-pointer">
+      <div className="border-2 p-2 text-text-dark rounded-2xl shadow-lg min-w-[160px] cursor-pointer">
         <TwitterShareButton url={shareUrl} className="flex items-center justify-between w-full">
           <TwitterIcon size={36} round />
         <p className="font-medium">Twitter</p>
